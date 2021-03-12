9b596a75b0f5e2f50164ccd7f6556a3cb0de7c44
matthew dennin <danny.dennin@steampunk.com>
2021-03-11 21:53:49 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ import LoginPage from './pages/loginPage/loginPage';
 //components
 import Header from './components/header/header';
 //utils
-import { auth } from './firebase/firebase';
+import { auth, createUserProfileDocument } from './firebase/firebase';
 function App() {
   const [currentUser, setCurrentUser] = useState();
   useEffect(()=>{
-    const unsubscribe = auth.onAuthStateChanged(user =>setCurrentUser(user));
-    console.log(currentUser);
+    const unsubscribe = auth.onAuthStateChanged(async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot=>{
+          console.log(`%ccurrent user snapshot: ${JSON.stringify(snapshot.data())}`, 'color: green');
+          setCurrentUser({...snapshot.data()})
+        });
+      }
+      setCurrentUser(null);
+    });
+    console.log(`currentUser: ${JSON.stringify(currentUser)}`);
     return unsubscribe;
   }, []);
   return (
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,7 +14,27 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+//
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+};
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const signInWithGoogle = () => auth.signInWithPopup(new firebase.auth.GoogleAuthProvider().setCustomParameters({prompt:'select_account'}));
