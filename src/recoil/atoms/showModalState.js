5fe3da09f6a72c39600e586728d1fada2335b064
matthew dennin <danny.dennin@steampunk.com>
2021-04-02 09:18:46 -0400

    update

--- src/recoil/atoms/showModalState.js
+++ src/recoil/atoms/showModalState.js
@@ -4,3 +4,7 @@ export const showModalState = atom({
     key: 'showModalState',
     default: false
 });
+export const addPetModalState = atom({
+    key: 'addPetModalState',
+    default: false
+});
